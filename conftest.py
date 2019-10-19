5d0c7d287b5ea0f08aa3d4dfa3c7078076e5bd14
Alexey Evdokimov <uncle-arthy@users.noreply.github.com>
2019-10-20 00:53:03 +0500

    Add implicitly waiting to browser

--- conftest.py
+++ conftest.py
@@ -27,6 +27,7 @@ def browser(request):
         browser = webdriver.Firefox(firefox_profile=fp)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
+    browser.implicitly_wait(5)
     yield browser
     print("\nquit browser..")
     browser.quit()
