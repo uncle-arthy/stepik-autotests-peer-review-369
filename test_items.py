c6465dec3cc897925f0f1d97416ef933dc42fb7e
Alexey Evdokimov <uncle-arthy@users.noreply.github.com>
2019-10-20 18:48:13 +0500

    Change test-method name to self-explanatory

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ import time
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
-def test_guest_should_see_login_link(browser):
+def test_add_to_basket_button_exist(browser):
     browser.get(link)
 
     # time.sleep(30)  # Little help for reviewer ;)
